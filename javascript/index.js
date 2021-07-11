7478eda27f335e33e4c3fdf51dab9e8fcc6576f2
074117097110080 <juanpablo1996epifani@gmail.com>
2021-07-10 22:10:08 -0300

    Trending Suggestions

--- javascript/index.js
+++ javascript/index.js
@@ -103,8 +103,9 @@ const cant = ()=>{
     indexMore.classList.add('hidden');
     deleteNode(conteiner);
     let A = document.getElementById('searchResult');
-    A.innerHTML = search.value;
-    
+    if(A.textContent == '') {
+        A.innerHTML = search.value;
+    }
     if(info.data.lenght == 0) {
         return
     }
@@ -486,6 +487,9 @@ const download = async (id, title) => {
 
 // ============================ //
 
+const endpointTrendingSearches = 'https://api.giphy.com/v1/trending/searches?api_key=';
+
+const trendingSuggestionContainer = document.getElementById('container-trending')
 
 function checkGifArray (gifArray) {
     if(gifArray == null){
@@ -498,3 +502,34 @@ function checkGifArray (gifArray) {
         }
     }
 }
+
+/* FUNCIÓN OBTENER SUGERENCIAS AL BUSCAR */
+const getTrendingSearches = async (amount) => {
+    try {
+      const response = await fetch(endpointTrendingSearches + 'T96v34LvfncPq5iV6LjP4GsHYqeQEupg');
+      const data = await response.json();
+      renderTrendingSearches(data.data.slice(0, amount));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+getTrendingSearches(6);
+
+/* FUNCIÓN RENDERIZAR SUGERENCIAS AL BUSCAR */
+const renderTrendingSearches = (data) => {
+    const fragment = new DocumentFragment();
+    data.forEach(element => {
+      const span = document.createElement('span');
+      span.setAttribute('class', 'trendingsugg__content-trend');
+      span.textContent = element;
+      fragment.appendChild(span);
+      span.addEventListener('click', () => {
+        let A = document.getElementById('searchResult');
+        A.textContent = element;
+        search.textContent = element;
+        newSearch(element);
+      });
+    });
+    trendingSuggestionContainer.appendChild(fragment);
+  };
